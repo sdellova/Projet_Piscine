8fe73ed3d4e4205efca2d1bbc10e57807f86fc05
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-27 17:55:55 +0200

    Create Sommet.h

---
+++ Sommet.h
@@ -0,0 +1,23 @@
+#ifndef SOMMET_H_INCLUDED
+#define SOMMET_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+class Sommet
+{
+public:
+    Sommet(int indice, std::string nom, double x, double y);
+    int getIndice();
+    std::string getNom();
+    double getX();
+    double getY();
+private:
+    int m_indice;
+    std::string m_nom;
+    double m_x;
+    double m_y;
+    std::vector<Sommet*> m_voisins;
+};
+
+#endif // SOMMET_H_INCLUDED
