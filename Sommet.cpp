5e810872a664e648494abfab6eb4a6f417d2cfde
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-27 17:55:35 +0200

    Create Sommet.cpp

---
+++ Sommet.cpp
@@ -0,0 +1,26 @@
+#include "Sommet.h"
+
+Sommet::Sommet(int indice, std::string nom, double x, double y) : m_indice{indice}, m_nom{nom}, m_x{x}, m_y{y}
+{
+
+}
+
+int Sommet::getIndice()
+{
+    return m_indice;
+}
+
+std::string Sommet::getNom()
+{
+    return m_nom;
+}
+
+double Sommet::getX()
+{
+    return m_x;
+}
+
+double Sommet::getY()
+{
+    return m_y;
+}
