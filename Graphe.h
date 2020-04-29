99d1d6724c07a2e439bf08239da25dc009680b3a
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-29 15:40:48 +0200

    Update Graphe.h

--- Graphe.h
+++ Graphe.h
@@ -6,21 +6,28 @@
 #include <string>
 #include "svgfile.h"
 #include "Sommet.h"
+#include "Arete.h"
 
 class Graphe
 {
 public:
     Graphe(std::string);
-    std::string ponderation();
+    void ponderation();
     void dessiner();
+    void menu();
+    void supprimerAretes();
     Sommet* getSommetByIndice(int indice);
+    Arete* getAreteByIndice(int indice);
+    int getOrdre() const;
+    int getTaille();
+    void centralite_degre();
+    void centralite_vecteur_propre();
 private:
     bool m_orientation;
     int m_ordre;
     int m_taille;
     std::vector<Sommet*> m_sommets;
-    std::vector<std::pair<Sommet*, Sommet*>> m_aretes;
-    std::vector<double> m_poids;
+    std::vector<Arete*> m_aretes;
 };
 
 #endif // GRAPHE_H_INCLUDED
