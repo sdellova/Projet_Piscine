17c8eabb93deab502367354e59bece1118535aad
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-29 16:11:53 +0200

    Update Sommet.h

--- Sommet.h
+++ Sommet.h
@@ -12,12 +12,15 @@ public:
     std::string getNom();
     double getX();
     double getY();
+    void setIndice_vecteur_propre(int indice_vecteur_propre);
+    int getIndice_vecteur_propre();
 private:
     int m_indice;
     std::string m_nom;
     double m_x;
     double m_y;
     std::vector<Sommet*> m_voisins;
+    int m_indice_vecteur_propre;
 };
 
 #endif // SOMMET_H_INCLUDED
