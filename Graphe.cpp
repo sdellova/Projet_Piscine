da2972c264bdfd8c44c241e4253c81f0652f5eeb
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-29 15:40:44 +0200

    Update Graphe.cpp

--- Graphe.cpp
+++ Graphe.cpp
@@ -18,25 +18,45 @@ Graphe::Graphe(std::string nomFichier)
     for(int i=0 ; i<m_taille ; ++i)
     {
         ifs >> indice >> extremite1 >> extremite2;
-        m_aretes.push_back(std::make_pair(getSommetByIndice(extremite1), getSommetByIndice(extremite2)));
+        m_aretes.push_back(new Arete{indice, getSommetByIndice(extremite1), getSommetByIndice(extremite2)});
     }
 }
 
-std::string Graphe::ponderation()
+void Graphe::ponderation()
 {
-    int choix;
+    int indice;
+    int choix = 0, taille, poids;
+    int choixTaille = 0;
     std::string nomFichier;
-    std::cout << "Le graphe est-il pondere ?" << std::endl;
-    std::cout << "Ecrivez 1 pour oui, 2 pour non : ";
-    std::cin >> choix;
-    if(choix)
+    std::cout << std::endl << "Le graphe est-il pondere ?" << std::endl;
+    while(choix != 1 && choix != 2)
+    {
+        std::cout << "Ecrivez 1 pour oui, 2 pour non : ";
+        std::cin >> choix;
+    }
+    if(choix == 1)
     {
         std::cout << "Ecrivez le nom du fichier de ponderation : ";
         std::cin >> nomFichier;
-        return nomFichier;
+        std::ifstream ifs{nomFichier};
+        if (!ifs)
+            throw std::runtime_error("Impossible d'ouvrir " + nomFichier);
+        ifs >> taille;
+        if (taille != m_taille)
+        {
+            std::cout << "La taille du graphe ne correspond pas entre les 2 fichiers." << std::endl;
+            std::cout << "Que garde-t-on ?" << std::endl;
+            std::cout << "1) " << m_taille << "  2) " << taille << std::endl;
+            std::cin >> choixTaille;
+            if (choixTaille == 2)
+                m_taille = taille;
+        }
+        for(int i=0 ; i<m_taille ; ++i)
+        {
+            ifs >> indice >> poids;
+            getAreteByIndice(indice)->setPoids(poids);
+        }
     }
-    else
-        return "0";
 }
 
 void Graphe::dessiner()
@@ -49,7 +69,7 @@ void Graphe::dessiner()
     }
     for(int i=0 ; i<m_taille ; ++i)
     {
-        svgout.addLine((m_aretes[i].first)->getX() * 100, (m_aretes[i].first)->getY() * 100, (m_aretes[i].second)->getX() * 100, (m_aretes[i].second)->getY() * 100);
+        svgout.addLine(((m_aretes[i]->getExtremites()).first)->getX() * 100, ((m_aretes[i]->getExtremites()).first)->getY() * 100, ((m_aretes[i]->getExtremites()).second)->getX() * 100, ((m_aretes[i]->getExtremites()).second)->getY() * 100);
     }
 }
 
@@ -61,4 +81,122 @@ Sommet* Graphe::getSommetByIndice(int indice)
         ++i;
     }
     return m_sommets[i];
+}
+
+Arete* Graphe::getAreteByIndice(int indice)
+{
+    int i = 0;
+    while(m_aretes[i]->getIndice() != indice)
+    {
+        ++i;
+    }
+    return m_aretes[i];
+}
+
+void Graphe::menu()
+{
+    int choix = 0;
+    while(choix != 1 && choix != 2 && choix != 3 && choix != 4 && choix != 5 && choix != 6 && choix != 7 && choix != 8 && choix != 9)
+    {
+        system("cls");
+        std::cout << "1) Calculer les indices de centralite de degre" << std::endl;
+        std::cout << "2) Calculer les indices de centralite de vecteur propre" << std::endl;
+        std::cout << "3) Calculer les indices de centralite de proximite" << std::endl << std::endl;
+        std::cout << "4) Supprimer des aretes du graphe" << std::endl << std::endl;
+        std::cout << "5) Charger un nouveau fichier de ponderation" << std::endl;
+        std::cout << "6) Quitter" << std::endl << std::endl;
+        std::cout << "Que choisissez-vous ? ";
+        std::cin >> choix;
+    }
+    switch(choix)
+    {
+    case 1:
+        centralite_degre();
+        break;
+    case 2 :
+        centralite_vecteur_propre();
+    case 4 :
+        supprimerAretes();
+        break;
+    case 5 :
+        ponderation();
+        break;
+    case 6 :
+        exit(1);
+        break;
+    }
+}
+
+void Graphe::supprimerAretes()
+{
+    int choix;
+    system("cls");
+    do
+    {
+        std::cout << "Indiquez l'indice de l'arete a supprimer : ";
+        std::cin >> choix;
+        delete getAreteByIndice(choix);
+        m_aretes.erase(m_aretes.begin() + choix);
+        dessiner();
+    }
+    while(choix!= 50);
+}
+
+int Graphe::getOrdre() const
+{
+    return m_ordre;
+}
+
+int Graphe::getTaille()
+{
+    return m_taille;
+}
+
+void Graphe::centralite_degre()
+{
+    std::vector<int> voisins;
+    float numberv=0;
+    std::vector<int> gauche;
+    std::vector<int> droite;
+    int a;
+    std::vector<Sommet*> sommets;
+    system("cls");
+    std::cout << "L'odre du graphe est de " << m_ordre << std::endl<< std::endl << std::endl;
+
+    for(int i=0 ; i<m_ordre ; ++i)
+    {
+        sommets.push_back(getSommetByIndice(i));
+    }
+    for(int i=0 ; i<m_taille ; ++i)
+    {
+        gauche.push_back(getAreteByIndice(i)->getExtremites().first->getIndice());
+        droite.push_back(getAreteByIndice(i)->getExtremites().second->getIndice());
+    }
+    for(int c=0; c<m_ordre; ++c)
+    {
+        voisins.push_back(numberv);
+    }
+
+    std::cout << "Choisir un sommet : ";
+    std::cin >> a;
+
+    for(int i=0 ; i<m_ordre ; ++i)
+    {
+        if(sommets[a]->getIndice()==gauche[i])
+        {
+            numberv++;
+        }
+        if(sommets[a]->getIndice()==droite[i])
+        {
+            numberv++;
+        }
+    }
+    std::cout << std::endl << "Le degre du sommet " << a << " est " << numberv << "." << std::endl;
+    float resultat=(numberv/(m_ordre-1));
+    std::cout << "L'indice normalise du sommet " << a << " est " << resultat << ".";
+}
+
+void Graphe::centralite_vecteur_propre()
+{
+
 }
