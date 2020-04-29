bd98e75c90092685c8d8d962ba4428bd9eff6096
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-29 15:40:38 +0200

    Create Arete.cpp

---
+++ Arete.cpp
@@ -0,0 +1,21 @@
+#include "Arete.h"
+
+Arete::Arete(int indice, Sommet* extremite1, Sommet* extremite2, float poids) : m_indice{indice}, m_extremites{std::make_pair(extremite1, extremite2)}, m_poids{poids}
+{
+
+}
+
+std::pair<Sommet*, Sommet*> Arete::getExtremites()
+{
+    return m_extremites;
+}
+
+int Arete::getIndice()
+{
+    return m_indice;
+}
+
+void Arete::setPoids(float poids)
+{
+    m_poids = poids;
+}
