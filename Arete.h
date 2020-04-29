39701a8152a0a90cbab12739708bd8037959f819
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-29 15:40:41 +0200

    Create Arete.h

---
+++ Arete.h
@@ -0,0 +1,20 @@
+#ifndef ARETE_H_INCLUDED
+#define ARETE_H_INCLUDED
+
+#include "Sommet.h"
+
+class Arete
+{
+public:
+    Arete(int indice, Sommet* extremite1, Sommet* extremite2, float poids = 1);
+    std::pair<Sommet*, Sommet*> getExtremites();
+    int getIndice();
+    void setPoids(float poids);
+private:
+    int m_indice;
+    std::pair<Sommet*, Sommet*> m_extremites;
+    float m_poids;
+};
+
+
+#endif // ARETE_H_INCLUDED
