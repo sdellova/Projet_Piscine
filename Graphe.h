c1d086f18ee268c3826d137f3dbb4cd6b5606de8
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-30 18:48:02 +0200

    Update Graphe.h

--- Graphe.h
+++ Graphe.h
@@ -31,6 +31,7 @@ public:
     int getPositionAreteByIndice(int indice);
     bool areteExistante(int indice);
     void indice_proximite(bool a);
+    void sauvegarde();
 private:
     bool m_orientation;
     int m_ordre;
