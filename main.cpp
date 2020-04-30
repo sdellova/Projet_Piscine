2b573acb7052907aa196ab3d98b51ecebca18be4
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-04-30 19:58:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     graphe.centralite_vecteur_propre(0);
     graphe.indice_proximite(0);
     graphe.sauvegarde();
-    graphe.dessiner();
+    graphe.dessiner(0);
     graphe.menu();
     return 0;
 }
