6072fa15a2fa4cd290d8f78740739c5fad75acd6
Sébastien Dell'Ova <56848984+sdellova@users.noreply.github.com>
2020-05-02 12:42:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 
 int main()
 {
-    std::string afficherMenu();
-    Graphe graphe(afficherMenu());
+    Graphe graphe(graphe.afficherMenu());
     graphe.ponderation();
     graphe.centralite_degre(0);
     graphe.centralite_vecteur_propre(0);
@@ -13,12 +12,4 @@ int main()
     graphe.dessiner(0);
     graphe.menu();
     return 0;
-}
-
-std::string afficherMenu()
-{
-    std::string nomFichier;
-    std::cout << "Ecrivez le nom du graphe a charger : ";
-    std::cin >> nomFichier;
-    return nomFichier;
 }
